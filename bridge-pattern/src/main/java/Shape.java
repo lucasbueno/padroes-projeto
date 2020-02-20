a97fd5d8f0e2fbbe91014585847a72ca3afe11ec
Lucas Bueno <lucasbuenolb@gmail.com>
2020-02-20 20:28:18 -0300

    início do bridge pattern

---
+++ bridge-pattern/src/main/java/Shape.java
@@ -0,0 +1,20 @@
+
+public abstract class Shape {
+	private String color;
+
+	public Shape(String color) {
+		super();
+		this.color = color;
+	}
+
+	public abstract void draw();
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
